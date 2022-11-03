87899c66edff8ec7f26dfcbfdfd6511bb32dca62
Samuel Oviedo <isaaccoviedo@gmail.com>
2022-11-03 16:07:37 -0600

    CRUD Proveedor

--- Conexion.cs
+++ Conexion.cs
@@ -12,9 +12,15 @@ namespace PulperiaPY
 {
     class Conexion
     {
-        //String connection = "Data Source=(local)\\SQLEXPRESS; Initial Catalog = pulperiaHermanos; Integrated Security= True";
-        String connection = "Server=tcp:pulperia.database.windows.net,1433;Initial Catalog=GrupoClinica;Persist Security Info=False;User ID=administrador;Password=Pulperia2022;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        String connection = "Data Source=DESKTOP-NQIVP0D; Initial Catalog = pulperiaproyect; Integrated Security= True";
+        //String connection = "Server=tcp:gestiong1.database.windows.net,1433;Initial Catalog=pulperiaproyect;Persist Security Info=False;User ID=AdminUnicah;Password=Gestiongrup01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
         public SqlConnection Conectar = new SqlConnection();
+        public SqlDataAdapter adaptador;
+        public DataTable tablaDatos;
+        public SqlDataReader lectorVariables;
+        public SqlCommand comando;
+
         public Conexion(){
             Conectar.ConnectionString = connection;
         }
@@ -42,6 +48,55 @@ namespace PulperiaPY
             {
                 Console.WriteLine("Existió un error al cerrar conexión:", e.Message);
             }
+        }
+
+        //Metodo enviar COMANDOS SQL donde se quiera hacer operaciones de SELECT, INSERT, DELETE, UPDATE, (se envia comando SQL)     [RETORNA null SI HAY UN ERROR DE EJECUCION]
+        public bool ejecutarComandoSQL(string instruccion)
+        {
+            try
+            {
+                Conectar.Open();
+                comando = new SqlCommand(instruccion, Conectar);
+                if (comando.ExecuteNonQuery() != -1)
+                {
+                    Conectar.Close();
+                    return true;
+                }
+                else
+                {
+                    Conectar.Close();
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Conectar.Close();
+                MessageBox.Show("Error al ejecutar comando SQL. " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+        }
+
+        //Agrega informacion de una consulta SQL a un datagridview
+        public bool llenarDGV(DataGridView dgv, string instruccion)
+        {
+            try
+            {
+                Conectar.Open();
+                adaptador = new SqlDataAdapter(instruccion, Conectar);
+                tablaDatos = new DataTable();
+                adaptador.Fill(tablaDatos);
+                dgv.DataSource = tablaDatos;
+                Conectar.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                Conectar.Close();
+                MessageBox.Show("Error en la conexión");
+                return false;
+            }
+            finally { Conectar.Close(); }
         }
     }
 }
---
+++ FrmProveedor.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PulperiaPY
+{
+    public partial class FrmProveedor : Form
+    {
+        Conexion conexionDb = new Conexion();
+        public int estadoBit { get; set; }
+        public FrmProveedor()
+        {
+            InitializeComponent();
+            CargarProveedores();
+        }
+        private void CargarProveedores()
+        {
+            conexionDb.llenarDGV(dgvProveedores, "Execute verProveedoresACT");
+        }
+        private void CargarProveedoresIna()
+        {
+            conexionDb.llenarDGV(dgvProveedores, "Execute verProveedoresINA");
+        }
+        private void limpiarSel()
+        {
+            foreach (Control c in this.Controls)
+            {
+
+                if (c is TextBox)
+
+                {
+
+                    c.Text = "";
+
+                    //Enfoco en el primer TextBox
+
+                    this.txbIdProv.Focus();
+
+                }
+
+            }
+            cmbEstProv.SelectedIndex = 0;
+        }
+        private void FrmProveedor_Load(object sender, EventArgs e)
+        {
+            cmbEstProv.SelectedIndex = 0;
+            cmbFiltroProv.SelectedIndex = 0;
+        }
+
+        private void btnAddProv_Click(object sender, EventArgs e)
+        {
+            string message = "¿Está seguro que desea insertar el proveedor?";
+            string caption = "Comprobación";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            if (txbNameProv.Text != "" && txbTelProv.Text != "" && txbDirProv.Text != "" && cmbEstProv.SelectedIndex != 0)
+            {
+                string nombreProov = string.Format(txbNameProv.Text);
+                string telefono = string.Format(txbTelProv.Text);
+                string direccion = string.Format(txbDirProv.Text);
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    if (conexionDb.ejecutarComandoSQL("Execute crearProveedor '" + nombreProov + "' , '" + telefono + "' , '" + direccion + "', '" + estadoBit + "' "))
+                    {
+                        MessageBox.Show("Proveedor insertado correctamente", "Guardado correctamente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        CargarProveedores();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al insertar proveedor", "Error al insertar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    limpiarSel();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese los datos solicitados", "Error al insertar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgvProveedores_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txbIdProv.Text = dgvProveedores.CurrentRow.Cells[0].Value.ToString();
+                txbNameProv.Text = dgvProveedores.CurrentRow.Cells[1].Value.ToString();
+                txbTelProv.Text = dgvProveedores.CurrentRow.Cells[2].Value.ToString();
+                txbDirProv.Text = dgvProveedores.CurrentRow.Cells[3].Value.ToString();
+                string valorCel = dgvProveedores.CurrentRow.Cells[4].Value.ToString();
+                bool numCel = Convert.ToBoolean(valorCel);
+                if (numCel)
+                {
+                    cmbEstProv.SelectedIndex = 1;
+                }
+                else
+                {
+                    cmbEstProv.SelectedIndex = 2;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error al seleccionar proveedor", "Error al seleccionar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCleProv_Click(object sender, EventArgs e)
+        {
+            limpiarSel();
+        }
+
+        private void btnUpdProv_Click(object sender, EventArgs e)
+        {
+            string nombreProov = string.Format(txbNameProv.Text);
+            string telefono = string.Format(txbTelProv.Text);
+            string direccion = string.Format(txbDirProv.Text);
+
+            string message = "¿Está seguro que desea actualizar el proveedor?";
+            string caption = "Comprobación";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            if (txbNameProv.Text != "" && txbTelProv.Text != "" && txbDirProv.Text != "" && cmbEstProv.SelectedIndex != 0)
+            {
+                int idProv = Int32.Parse(txbIdProv.Text);
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    if (conexionDb.ejecutarComandoSQL("Execute actualizarProveedor '" + idProv + "' , '" + nombreProov + "' , '" + telefono + "' , '" + direccion + "', '" + estadoBit + "' "))
+                    {
+                        MessageBox.Show("Proveedor actualizado correctamente", "Actualizado correctamente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        CargarProveedores();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al actualizar proveedor", "Error al actualizar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    limpiarSel();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un proveedor", "Error al actualizar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearProv_Click(object sender, EventArgs e)
+        {
+            string estadoFil = cmbFiltroProv.Text;
+
+            if (txbSearProv.Text != "" && estadoFil == "Activos")
+            {
+                string nombreProov = string.Format(txbSearProv.Text);
+                if (conexionDb.llenarDGV(dgvProveedores, "Execute buscarProveedor '" + nombreProov + "'"))
+                {
+                    //Proveedor encontrado
+                }
+                else
+                {
+                    MessageBox.Show("Error al buscar proveedor", "Error al buscar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (txbSearProv.Text != "" && estadoFil == "Inactivos")
+            {
+                string nombreProov = string.Format(txbSearProv.Text);
+                if (conexionDb.llenarDGV(dgvProveedores, "Execute buscarProveedorIna '" + nombreProov + "'"))
+                {
+                    //Proveedor encontrado
+                }
+                else
+                {
+                    MessageBox.Show("Error al buscar proveedor", "Error al buscar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un nombre del proveedor", "Error al buscar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txbSearProv_TextChanged(object sender, EventArgs e)
+        {
+            string estadoFil = cmbFiltroProv.Text;
+
+            if (estadoFil == "Activos")
+            {
+                string nombreProov = string.Format(txbSearProv.Text);
+                if (conexionDb.llenarDGV(dgvProveedores, "Execute buscarProveedor '" + nombreProov + "'"))
+                {
+                    //Proveedor encontrado
+                }
+                else
+                {
+                    MessageBox.Show("Error al buscar proveedor", "Error al buscar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (estadoFil == "Inactivos")
+            {
+                string nombreProov = string.Format(txbSearProv.Text);
+                if (conexionDb.llenarDGV(dgvProveedores, "Execute buscarProveedorIna '" + nombreProov + "'"))
+                {
+                    //Proveedor encontrado
+                }
+                else
+                {
+                    MessageBox.Show("Error al buscar proveedor", "Error al buscar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void cmbEstProv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string estado = cmbEstProv.Text;
+
+            if (estado == "Activo")
+            {
+                estadoBit = 1;
+            }
+            else
+            {
+                estadoBit = 0;
+            }
+        }
+
+        private void btnDelProve_Click(object sender, EventArgs e)
+        {
+            string message = "¿Está seguro que desea eliminar el proveedor?";
+            string caption = "Comprobación";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            if (txbIdProv.Text != "")
+            {
+                int idProv = Int32.Parse(txbIdProv.Text);
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    if (conexionDb.ejecutarComandoSQL("Execute eliminarProveedor '" + idProv + "'"))
+                    {
+                        MessageBox.Show("Proveedor eliminado correctamente", "Eliminado correctamente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        CargarProveedores();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al eliminar proveedor", "Error al eliminar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    limpiarSel();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un proveedor", "Error al eliminar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmbFiltroProv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txbSearProv.Text = "";
+            string estadoFil = cmbFiltroProv.Text;
+
+            if (estadoFil == "Activos")
+            {
+                CargarProveedores();
+            }
+            else
+            {
+                CargarProveedoresIna();
+            }
+        }
+    }
+}
