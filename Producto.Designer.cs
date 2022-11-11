3463186d287ce18395d6469e3d6a83c077874602
Javier C <acej2207@gmail.com>
2022-11-10 19:23:13 -0600

    primer prueba de agregar producto

---
+++ Producto.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PulperiaPY
+{
+    partial class Producto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.categoriacb = new System.Windows.Forms.ComboBox();
+            this.marcacb = new System.Windows.Forms.ComboBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marca";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoría";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Código de Producto";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nombre de Producto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio Sugerido";
+            // 
+            // categoriacb
+            // 
+            this.categoriacb.FormattingEnabled = true;
+            this.categoriacb.Location = new System.Drawing.Point(276, 92);
+            this.categoriacb.Name = "categoriacb";
+            this.categoriacb.Size = new System.Drawing.Size(111, 24);
+            this.categoriacb.TabIndex = 5;
+            // 
+            // marcacb
+            // 
+            this.marcacb.FormattingEnabled = true;
+            this.marcacb.Location = new System.Drawing.Point(276, 59);
+            this.marcacb.Name = "marcacb";
+            this.marcacb.Size = new System.Drawing.Size(111, 24);
+            this.marcacb.TabIndex = 6;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(276, 128);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(111, 22);
+            this.txtCodigo.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(276, 158);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(111, 22);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(276, 186);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(111, 22);
+            this.txtPrecio.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(203, 239);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 32);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(203, 277);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 32);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(203, 315);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 32);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 24);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Nueva Categoria";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(394, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 24);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Nueva Marca";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Producto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.marcacb);
+            this.Controls.Add(this.categoriacb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Producto";
+            this.Text = "Producto";
+            this.Load += new System.EventHandler(this.Producto_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox categoriacb;
+        private System.Windows.Forms.ComboBox marcacb;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PulperiaPY
+{
+    public partial class Producto : Form
+    {
+        Conexion conexion = new Conexion();
+        
+        public Producto()
+        {
+            InitializeComponent();
+            cargarMarcas();
+            cargarCategoria();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Producto_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cargarMarcas()
+        {
+            
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = conexion.connection;
+                SqlCommand sqlComm = new SqlCommand("MostrarMarca", conn);
+                sqlComm.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = sqlComm;
+                DataTable result = new DataTable();
+                da.Fill(result);
+                marcacb.DisplayMember = "Nombre Marca";
+                marcacb.ValueMember = "Codigo Marca";
+                marcacb.DataSource = result;
+                marcacb.Refresh();
+            }
+        }
+
+        private void cargarCategoria()
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = conexion.connection;
+                SqlCommand sqlComm = new SqlCommand("MostrarCategoria", conn);
+                sqlComm.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = sqlComm;
+                DataTable result = new DataTable();
+                da.Fill(result);
+
+                categoriacb.DataSource = result;
+                categoriacb.DisplayMember = "nombreCategoria";
+                categoriacb.ValueMember = "idCategoria";
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = conexion.connection;
+                conn.Open();
+                SqlCommand sqlComm = new SqlCommand("InsertarProducto", conn);
+                sqlComm.CommandType = CommandType.StoredProcedure;
+                sqlComm.Parameters.AddWithValue("@IdMarca", Int32.Parse(marcacb.SelectedValue.ToString()));
+                sqlComm.Parameters.AddWithValue("@IdCategoria", Int32.Parse(categoriacb.SelectedValue.ToString()));
+                sqlComm.Parameters.AddWithValue("@CodigoProducto", Int64.Parse(txtCodigo.Text));
+                sqlComm.Parameters.AddWithValue("@NombreProducto", txtNombre.Text);
+                sqlComm.Parameters.AddWithValue("@PrecioSugerido", txtPrecio.Text);
+                sqlComm.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Producto agregado con éxito");
+                Limpiar();
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
