b152cdedc497e3f16c74634de129eab82e7edac9
Samuel Oviedo <isaaccoviedo@gmail.com>
2022-11-11 18:04:32 -0600

    cambio en tamaño

--- FrmProveedor.Designer.cs
+++ FrmProveedor.Designer.cs
@@ -48,69 +48,72 @@ namespace PulperiaPY
             this.btnDelProve = new System.Windows.Forms.Button();
             this.cmbFiltroProv = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 16);
+            this.label1.Size = new System.Drawing.Size(147, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre proveedor:";
             // 
             // txbNameProv
             // 
-            this.txbNameProv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbNameProv.Location = new System.Drawing.Point(31, 45);
+            this.txbNameProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNameProv.Location = new System.Drawing.Point(50, 92);
             this.txbNameProv.Name = "txbNameProv";
-            this.txbNameProv.Size = new System.Drawing.Size(163, 22);
+            this.txbNameProv.Size = new System.Drawing.Size(258, 29);
             this.txbNameProv.TabIndex = 1;
             // 
             // txbTelProv
             // 
-            this.txbTelProv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTelProv.Location = new System.Drawing.Point(224, 45);
+            this.txbTelProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTelProv.Location = new System.Drawing.Point(368, 92);
             this.txbTelProv.Name = "txbTelProv";
-            this.txbTelProv.Size = new System.Drawing.Size(100, 22);
+            this.txbTelProv.Size = new System.Drawing.Size(153, 29);
             this.txbTelProv.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(221, 26);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(364, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.Size = new System.Drawing.Size(71, 21);
             this.label2.TabIndex = 2;
             this.label2.Text = "Teléfono:";
             // 
             // txbDirProv
             // 
-            this.txbDirProv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbDirProv.Location = new System.Drawing.Point(356, 45);
+            this.txbDirProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDirProv.Location = new System.Drawing.Point(579, 92);
             this.txbDirProv.Multiline = true;
             this.txbDirProv.Name = "txbDirProv";
-            this.txbDirProv.Size = new System.Drawing.Size(221, 91);
+            this.txbDirProv.Size = new System.Drawing.Size(360, 137);
             this.txbDirProv.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(353, 26);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(575, 59);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.Size = new System.Drawing.Size(75, 21);
             this.label3.TabIndex = 4;
             this.label3.Text = "Dirección";
             // 
             // btnAddProv
             // 
-            this.btnAddProv.Location = new System.Drawing.Point(602, 12);
+            this.btnAddProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddProv.Location = new System.Drawing.Point(132, 51);
             this.btnAddProv.Name = "btnAddProv";
-            this.btnAddProv.Size = new System.Drawing.Size(110, 34);
+            this.btnAddProv.Size = new System.Drawing.Size(131, 48);
             this.btnAddProv.TabIndex = 6;
             this.btnAddProv.Text = "Ingresar";
             this.btnAddProv.UseVisualStyleBackColor = true;
@@ -118,9 +121,10 @@ namespace PulperiaPY
             // 
             // btnUpdProv
             // 
-            this.btnUpdProv.Location = new System.Drawing.Point(602, 52);
+            this.btnUpdProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdProv.Location = new System.Drawing.Point(132, 135);
             this.btnUpdProv.Name = "btnUpdProv";
-            this.btnUpdProv.Size = new System.Drawing.Size(110, 34);
+            this.btnUpdProv.Size = new System.Drawing.Size(131, 48);
             this.btnUpdProv.TabIndex = 7;
             this.btnUpdProv.Text = "Actualizar";
             this.btnUpdProv.UseVisualStyleBackColor = true;
@@ -128,9 +132,10 @@ namespace PulperiaPY
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(602, 147);
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1311, 729);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(110, 34);
+            this.button3.Size = new System.Drawing.Size(131, 48);
             this.button3.TabIndex = 9;
             this.button3.Text = "Reporte";
             this.button3.UseVisualStyleBackColor = true;
@@ -142,19 +147,20 @@ namespace PulperiaPY
             this.dgvProveedores.AllowUserToResizeRows = false;
             this.dgvProveedores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProveedores.Location = new System.Drawing.Point(31, 194);
+            this.dgvProveedores.Location = new System.Drawing.Point(50, 514);
             this.dgvProveedores.MultiSelect = false;
             this.dgvProveedores.Name = "dgvProveedores";
             this.dgvProveedores.ReadOnly = true;
-            this.dgvProveedores.Size = new System.Drawing.Size(681, 233);
+            this.dgvProveedores.Size = new System.Drawing.Size(1200, 456);
             this.dgvProveedores.TabIndex = 10;
             this.dgvProveedores.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProveedores_CellContentClick);
             // 
             // btnSearProv
             // 
-            this.btnSearProv.Location = new System.Drawing.Point(379, 159);
+            this.btnSearProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearProv.Location = new System.Drawing.Point(1139, 468);
             this.btnSearProv.Name = "btnSearProv";
-            this.btnSearProv.Size = new System.Drawing.Size(75, 23);
+            this.btnSearProv.Size = new System.Drawing.Size(110, 31);
             this.btnSearProv.TabIndex = 11;
             this.btnSearProv.Text = "Buscar";
             this.btnSearProv.UseVisualStyleBackColor = true;
@@ -162,16 +168,17 @@ namespace PulperiaPY
             // 
             // txbSearProv
             // 
-            this.txbSearProv.Location = new System.Drawing.Point(185, 161);
+            this.txbSearProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSearProv.Location = new System.Drawing.Point(787, 468);
             this.txbSearProv.Name = "txbSearProv";
-            this.txbSearProv.Size = new System.Drawing.Size(188, 20);
+            this.txbSearProv.Size = new System.Drawing.Size(337, 29);
             this.txbSearProv.TabIndex = 12;
             this.txbSearProv.TextChanged += new System.EventHandler(this.txbSearProv_TextChanged);
             // 
             // txbIdProv
             // 
             this.txbIdProv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbIdProv.Location = new System.Drawing.Point(3, 1);
+            this.txbIdProv.Location = new System.Drawing.Point(19, 15);
             this.txbIdProv.Name = "txbIdProv";
             this.txbIdProv.ReadOnly = true;
             this.txbIdProv.Size = new System.Drawing.Size(45, 22);
@@ -180,7 +187,8 @@ namespace PulperiaPY
             // 
             // btnCleProv
             // 
-            this.btnCleProv.Location = new System.Drawing.Point(485, 147);
+            this.btnCleProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCleProv.Location = new System.Drawing.Point(429, 195);
             this.btnCleProv.Name = "btnCleProv";
             this.btnCleProv.Size = new System.Drawing.Size(92, 34);
             this.btnCleProv.TabIndex = 15;
@@ -191,32 +199,34 @@ namespace PulperiaPY
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(28, 83);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 147);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.Size = new System.Drawing.Size(59, 21);
             this.label4.TabIndex = 16;
             this.label4.Text = "Estado:";
             // 
             // cmbEstProv
             // 
             this.cmbEstProv.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEstProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbEstProv.FormattingEnabled = true;
             this.cmbEstProv.Items.AddRange(new object[] {
             "Seleccione",
             "Activo",
             "Inactivo"});
-            this.cmbEstProv.Location = new System.Drawing.Point(31, 105);
+            this.cmbEstProv.Location = new System.Drawing.Point(50, 187);
             this.cmbEstProv.Name = "cmbEstProv";
-            this.cmbEstProv.Size = new System.Drawing.Size(101, 21);
+            this.cmbEstProv.Size = new System.Drawing.Size(117, 29);
             this.cmbEstProv.TabIndex = 17;
             this.cmbEstProv.SelectedIndexChanged += new System.EventHandler(this.cmbEstProv_SelectedIndexChanged);
             // 
             // btnDelProve
             // 
-            this.btnDelProve.Location = new System.Drawing.Point(602, 97);
+            this.btnDelProve.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelProve.Location = new System.Drawing.Point(132, 213);
             this.btnDelProve.Name = "btnDelProve";
-            this.btnDelProve.Size = new System.Drawing.Size(110, 34);
+            this.btnDelProve.Size = new System.Drawing.Size(131, 48);
             this.btnDelProve.TabIndex = 19;
             this.btnDelProve.Text = "Eliminar";
             this.btnDelProve.UseVisualStyleBackColor = true;
@@ -225,35 +235,49 @@ namespace PulperiaPY
             // cmbFiltroProv
             // 
             this.cmbFiltroProv.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFiltroProv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbFiltroProv.FormattingEnabled = true;
             this.cmbFiltroProv.Items.AddRange(new object[] {
             "Activos",
             "Inactivos",
             "Todos"});
-            this.cmbFiltroProv.Location = new System.Drawing.Point(31, 161);
+            this.cmbFiltroProv.Location = new System.Drawing.Point(217, 472);
             this.cmbFiltroProv.Name = "cmbFiltroProv";
-            this.cmbFiltroProv.Size = new System.Drawing.Size(101, 21);
+            this.cmbFiltroProv.Size = new System.Drawing.Size(123, 29);
             this.cmbFiltroProv.TabIndex = 20;
             this.cmbFiltroProv.SelectedIndexChanged += new System.EventHandler(this.cmbFiltroProv_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(28, 140);
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(46, 476);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(136, 16);
+            this.label5.Size = new System.Drawing.Size(159, 21);
             this.label5.TabIndex = 21;
             this.label5.Text = "Mostrar proveedores:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddProv);
+            this.groupBox1.Controls.Add(this.btnUpdProv);
+            this.groupBox1.Controls.Add(this.btnDelProve);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(1028, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 324);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Acciones";
             // 
             // FrmProveedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 444);
+            this.ClientSize = new System.Drawing.Size(1471, 1011);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.cmbFiltroProv);
-            this.Controls.Add(this.btnDelProve);
             this.Controls.Add(this.cmbEstProv);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnCleProv);
@@ -262,8 +286,6 @@ namespace PulperiaPY
             this.Controls.Add(this.btnSearProv);
             this.Controls.Add(this.dgvProveedores);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.btnUpdProv);
-            this.Controls.Add(this.btnAddProv);
             this.Controls.Add(this.txbDirProv);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txbTelProv);
@@ -276,6 +298,7 @@ namespace PulperiaPY
             this.Text = "FrmProveedor";
             this.Load += new System.EventHandler(this.FrmProveedor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -302,5 +325,6 @@ namespace PulperiaPY
         private System.Windows.Forms.Button btnDelProve;
         private System.Windows.Forms.ComboBox cmbFiltroProv;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
